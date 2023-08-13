1f1563ff64c173000bab352e6a2b362e290ba742
Shoya Ito <shoya.a.ito@avanade.com>
2023-08-13 21:23:55 +0900

    ニューゲーム遷移

--- Assets/Scripts/title_newgame.cs
+++ Assets/Scripts/title_newgame.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class title_newgame : MonoBehaviour
 {
     public void ClickStartButton()
     {
-        SceneManager.LoadScene("Title1");
+        Debug.Log("Scene: Town1");
+        SceneManager.LoadScene("Town1");
     }
 }
